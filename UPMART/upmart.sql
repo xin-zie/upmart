bf99bbdffd7ded1158cbc01f2c92b75ccd0cef9c
dianee <dianeprincess817@gmail.com>
2026-05-02 15:47:18 +0800

    Add files via upload

---
+++ UPMART/upmart.sql
@@ -0,0 +1,526 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 02, 2026 at 09:46 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `upmart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bulletin_posts`
+--
+
+CREATE TABLE `bulletin_posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `message` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bulletin_posts`
+--
+
+INSERT INTO `bulletin_posts` (`id`, `user_id`, `message`, `created_at`) VALUES
+(1, 10, 'sigeee na oh', '2026-04-23 15:09:58'),
+(2, 10, 'lf feetpixs', '2026-04-23 15:10:30'),
+(3, 18, 'lf manila plane ticket', '2026-04-23 15:16:32'),
+(4, 21, 'lf kasama sa lake cebu', '2026-04-23 15:19:04'),
+(5, 10, 'hanayko', '2026-04-23 15:19:34'),
+(6, 21, 'its 3:00 am', '2026-04-24 19:20:01'),
+(7, 21, 'juukggk', '2026-04-24 19:25:24'),
+(8, 21, 'good evening', '2026-04-28 15:59:25'),
+(9, 10, 'good morning', '2026-04-29 00:12:27'),
+(10, 10, 'hi pls sana matapos na ito', '2026-04-30 06:58:12'),
+(11, 10, 'dwdwdw', '2026-04-30 07:03:37'),
+(12, 10, 'dsdw', '2026-04-30 07:07:18'),
+(13, 10, 'MYGHAD', '2026-04-30 08:06:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+(1, 'Dorm Essentials'),
+(2, 'Arki Mats'),
+(3, 'Lab Essentials'),
+(4, 'Others');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inquiries`
+--
+
+CREATE TABLE `inquiries` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `status` enum('Pending','Resolved') DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media`
+--
+
+CREATE TABLE `media` (
+  `image_id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `image_path` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `media`
+--
+
+INSERT INTO `media` (`image_id`, `product_id`, `image_path`) VALUES
+(1, 4, 'uploads/1777653870_tombits.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `message_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `is_read` tinyint(4) DEFAULT 0,
+  `sent_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `notif_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `is_read` tinyint(1) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `buyer_id` int(11) DEFAULT NULL,
+  `seller_id` int(11) DEFAULT NULL,
+  `status` enum('Pending','Confirmed','Completed','Cancelled') DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `product_id`, `buyer_id`, `seller_id`, `status`, `created_at`) VALUES
+(1, 4, 10, 10, 'Pending', '2026-05-01 17:03:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `id` int(11) NOT NULL,
+  `up_email` varchar(100) NOT NULL,
+  `status` enum('Pending','Resolved') DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `seller_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `title` varchar(150) NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `item_condition` enum('New','Used') NOT NULL,
+  `status` enum('Available','Sold','Reserved') DEFAULT 'Available',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `approval_status` enum('Pending','Approved','Denied') DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `seller_id`, `category_id`, `title`, `description`, `price`, `item_condition`, `status`, `created_at`, `approval_status`) VALUES
+(4, 10, 4, 'othentic escrambol', 'baybeh di magiging sayooo yung para sa street', 500.00, 'New', 'Available', '2026-05-01 17:12:47', 'Approved');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `report_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `reason` varchar(50) NOT NULL,
+  `details` text NOT NULL,
+  `status` enum('Pending','Reviewed','Resolved') DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`report_id`, `user_id`, `reason`, `details`, `status`, `created_at`) VALUES
+(1, 13, 'scam', 'dwqfefef', 'Pending', '2026-04-28 15:43:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `inquiry_id` int(11) NOT NULL,
+  `buyer_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `message_link_clicked` tinyint(1) DEFAULT 0,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `up_email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  `bio` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `reset_code` varchar(64) DEFAULT NULL,
+  `reset_expires` datetime DEFAULT NULL,
+  `profile_pic` varchar(255) DEFAULT NULL,
+  `role` enum('student','admin') DEFAULT 'student',
+  `is_setup_complete` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `full_name`, `up_email`, `password`, `phone_number`, `bio`, `created_at`, `reset_code`, `reset_expires`, `profile_pic`, `role`, `is_setup_complete`) VALUES
+(4, 'Fiona Divine', 'Divine@up.edu.ph', '$2y$10$Y/KfN8/.YxRrDEhVdyvJROjWh2hE2QQ4B2ly0egTf9kCRHq.k79wS', NULL, NULL, '2026-04-21 16:52:59', NULL, NULL, NULL, 'student', 0),
+(6, 'Glendy', 'Glen@up.edu.ph', '$2y$10$Majwwx3EkRW9eMpdzHDASundpEyzPZBBpZAO1IZzlW7f0GiIvsdDq', NULL, NULL, '2026-04-21 17:15:20', NULL, NULL, NULL, 'student', 0),
+(10, 'Natasha Christine', 'Arroyo@up.edu.ph', '$2y$10$nxVoe11g8EwYyOhF785pC.FlNHwFK1BxvqZdJIAy9fbTiB0DIWPCC', '09758365262', 'i love feet pixs', '2026-04-21 17:27:56', NULL, NULL, 'user_10_1777613602.jpg', 'student', 1),
+(13, 'Princess Diane', 'Mahusay@up.edu.ph', '$2y$10$fg2kpt7VkqSAT5nM9/sG0.kAa0uuclr7teLkDXGr1keiMkmHw4/gq', NULL, NULL, '2026-04-21 17:37:11', NULL, NULL, NULL, 'student', 0),
+(15, 'Rhea Catacutan', 'Rhea@up.edu.ph', '$2y$10$Ws3ivVjtUGINxvzV2ER6zeb6JRIMBkOR64yu/.s0xeIfhfeKjPcKi', NULL, NULL, '2026-04-22 07:58:42', NULL, NULL, NULL, 'student', 0),
+(18, 'Diane', 'pcmahusay@up.edu.ph', '$2y$10$6cv2kg2s0owL/zhDbil0yen4kFDfMsNgZSR8/vft4L/nGdG8yAoF2', NULL, NULL, '2026-04-22 13:12:15', NULL, NULL, NULL, 'student', 0),
+(21, 'Sybil Micarandayo', 'scmicarandayo@up.edu.ph', '$2y$10$Jv4lpm4ZWgqo6sky69tdKOM7OXHrusPfDMLgjMcP4/QLfFRtNr3Qm', NULL, NULL, '2026-04-23 04:49:46', NULL, NULL, NULL, 'admin', 0),
+(28, 'Diane Mahusay', 'dianem@up.edu.ph', '$2y$10$bfgAhTn1JrcTMChC2kGe/O0jgJtggfn9loD6BaTIM0ZN584OELnN2', NULL, NULL, '2026-04-30 17:27:02', NULL, NULL, NULL, 'student', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+
+CREATE TABLE `wishlist` (
+  `wish_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `item_name` varchar(100) DEFAULT NULL,
+  `category` enum('Books','Dorm','Food','Electronics','Other') DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`wish_id`, `user_id`, `item_name`, `category`, `created_at`) VALUES
+(1, 21, 'ddwdqf', 'Dorm', '2026-04-28 10:48:13'),
+(2, 13, 'buldak', 'Food', '2026-04-28 15:43:24'),
+(3, 10, 'bondpaper', 'Other', '2026-04-29 00:13:15'),
+(4, 10, 'TIRAMIUSSIEE', 'Food', '2026-04-30 06:50:43'),
+(5, 10, 'Electric Cooker', 'Electronics', '2026-04-30 06:57:34'),
+(6, 10, 'Nails', 'Other', '2026-04-30 07:06:59'),
+(7, 10, 'Calculus', 'Books', '2026-04-30 08:06:01');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bulletin_posts`
+--
+ALTER TABLE `bulletin_posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `inquiries`
+--
+ALTER TABLE `inquiries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `media`
+--
+ALTER TABLE `media`
+  ADD PRIMARY KEY (`image_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`message_id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`notif_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `seller_id` (`seller_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`inquiry_id`),
+  ADD KEY `buyer_id` (`buyer_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `up_email` (`up_email`);
+
+--
+-- Indexes for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD PRIMARY KEY (`wish_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bulletin_posts`
+--
+ALTER TABLE `bulletin_posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `inquiries`
+--
+ALTER TABLE `inquiries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `media`
+--
+ALTER TABLE `media`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `notif_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `inquiry_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  MODIFY `wish_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bulletin_posts`
+--
+ALTER TABLE `bulletin_posts`
+  ADD CONSTRAINT `bulletin_posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+--
+-- Constraints for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
